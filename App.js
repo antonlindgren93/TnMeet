6351ccf68b4a07ba1510b57c5f90973fca81231c
CyrilleKolle <ngidekollecyrille@gmail.com>
2020-09-08 11:36:30 +0200

    removed boiler plate in app.js and create signIn as initial route

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import SignIn from './src/screens/SignIn'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigator = createStackNavigator({
+
+  SignIn: SignIn
+
+}, {
+  initialRouteName: 'SignIn',
+  defaultNavigationOptions: {
+    title: 'Sign In'
+  }
+})
+
+export default createAppContainer(navigator)
+
